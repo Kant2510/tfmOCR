559ade5726b9c3850a75c6c08ce01a6edcdf3319
tuannhat <tuannhat1209@gmail.com>
2023-11-08 16:22:14 +0700

    'transformer'

---
+++ tool/config.py
@@ -0,0 +1,17 @@
+import yaml
+
+class Cfg(dict):
+    def __init__(self, config_dict):
+        super(Cfg, self).__init__(**config_dict)
+        self.__dict__ = self
+
+    @staticmethod
+    def load_config_from_file(fname='default.yml'):
+        with open(fname, encoding='utf-8') as f:
+            config = yaml.safe_load(f)
+        return Cfg(config)
+
+    def save(self, fname):
+        with open(fname, 'w') as outfile:
+            yaml.dump(dict(self), outfile, default_flow_style=False, allow_unicode=True)
+
