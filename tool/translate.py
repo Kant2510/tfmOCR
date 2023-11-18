eba1634b60dd4257b218dbfb74873d0d279c1e85
tuannhat <tuannhat1209@gmail.com>
2023-11-18 10:02:19 +0700

    update dataset processing

--- tool/translate.py
+++ tool/translate.py
@@ -16,9 +16,9 @@ def batch_translate_beam_search(img, model, beam_size=4, candidates=1, max_seq_l
 
     with torch.no_grad():
         src = model.cnn(img)
-        print(src.shap) #fix
+        # print(src.shap) #fix
         memories = model.transformer.forward_encoder(src)
-        for i in range(src.size(0)):
+        for i in range(src.size(1)):
 #            memory = memories[:,i,:].repeat(1, beam_size, 1) # TxNxE
             memory = model.transformer.get_memory(memories, i)
             sent = beamsearch(memory, model, device, beam_size, candidates, max_seq_length, sos_token, eos_token)
